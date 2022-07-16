e5c48a537f811025a5466d24f44f4028b5cac8bc
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2022-07-16 18:58:24 +0300

    Refactoring: moving terrain_t to game namespace

---
+++ src/game/battle/state.hpp
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_STATE_HPP
+#define INCLUDE_STATE_HPP
+
+#include "game/terrain.hpp"
+
+namespace game::battle
+{
+    extern terrain_t terrain;
+}
+
+#endif // INCLUDE_STATE_HPP
---
+++ src/game/terrain.hpp
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_TERRAIN_HPP
+#define INCLUDE_TERRAIN_HPP
+
+namespace game
+{
+    enum terrain_t
+    {
+        FOREST,
+        MEADOW,
+        BEACH
+    };
+}
+
+#endif // INCLUDE_TERRAIN_HPP
